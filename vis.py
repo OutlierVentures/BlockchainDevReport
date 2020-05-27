dcc7163bb59aed2fc061b06f0a17ad6f47a3243b
Theo Turner <theo_turner@outlook.com>
2020-05-27 12:51:19 +0100

    Vis updates

--- vis.py
+++ vis.py
@@ -6,11 +6,11 @@ from dateutil.relativedelta import relativedelta
 class Visualize:
 
     def __init__(self):
-        self.chains = ['fetchai', 'ethereum']
+        self.chains = ['fetchai', 'ethereum', 'bitcoin']
         end = datetime.now()
         start = datetime.now() - relativedelta(years = 1)
         date_index = pd.date_range(start, end, freq = 'W')
-        self.df = pd.DataFrame({'date': date_index})
+        self.df = pd.DataFrame({'Date': date_index})
 
     def load(self, name : str):
         with open(name + '_history.json') as json_file:
@@ -19,8 +19,8 @@ class Visualize:
 
     def plot(self):
         sns.set(style="darkgrid")
-        df = self.df.melt('date', var_name = 'protocol',  value_name = 'commits')
-        fig = sns.lineplot(x = "date", y = "commits", hue = 'protocol', data = df)
+        df = self.df.melt('Date', var_name = 'Protocol',  value_name = 'Commits')
+        fig = sns.lineplot(x = "Date", y = "Commits", hue = 'Protocol', data = df)
         fig.get_figure().savefig('commits.png')
     
     def run(self):
