d30e87053725efd3edb8dd66c60396e0e237a515
Theo Turner <theo_turner@outlook.com>
2020-06-01 16:26:56 +0100

    Visualization: major refactor

--- vis.py
+++ vis.py
@@ -1,6 +1,7 @@
-import seaborn as sns, pandas as pd, json
+import seaborn as sns, pandas as pd, json, sys
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
+from matplotlib import pyplot as plt
 
 
 class Visualize:
@@ -10,41 +11,55 @@ class Visualize:
         self.target_names = ['Binance Chain', 'Bitcoin', 'Bitcoin Cash', 'Bitcoin SV', 'Corda', 'Cosmos', 'Crypto.Com', 'Dash', 'Dogecoin', 'EOS', 'Ethereum', 'Ethereum Classic', 'Huobi Chain', 'Hyperledger', 'Cardano', 'IOTA', 'Litecoin', 'MakerDAO', 'Monero', 'NEM', 'NEO', 'OKChain', 'Ontology', 'Polkadot', 'Ripple', 'Chainlink', 'Stellar', 'Theta', 'Tron', 'VeChain', 'Zcash']
         self.contributor_chains = ['bitcoin-cash', 'bitcoin', 'cardano', 'corda', 'cosmos', 'eos', 'ethereum', 'hyperledger', 'polkadot', 'ripple', 'stellar', 'tron']
         self.contributor_target_names = ['Bitcoin Cash', 'Bitcoin', 'Cardano', 'Corda', 'Cosmos', 'EOS', 'Ethereum', 'Hyperledger', 'Polkadot', 'Ripple', 'Stellar', 'Tron']
+        self.xaxis = ['Jun 19', 'Jul 19', 'Aug 19', 'Sep 19', 'Oct 19', 'Nov 19', 'Dec 19', 'Jan 20', 'Feb 20', 'Mar 20', 'Apr 20', 'May 20']
         end = datetime.now()
         start = datetime.now() - relativedelta(years = 1)
         date_index = pd.date_range(start, end, freq = 'W')
+        if len(date_index) > 52:
+            date_index = date_index.drop(date_index[-1])
         self.commits = pd.DataFrame({'Date': date_index})
         self.churn = pd.DataFrame({'Date': date_index})
-
-    def load(self, name : str):
-        with open(name + '_history.json') as json_file:
-            data = json.load(json_file)
-        self.commits[name] = data['weekly_commits']
-        self.churn[name] = data['weekly_churn'][-52:]
-
-    def plot_commits(self):
-        # Compute a 4-period MA to smooth data
         for chain in self.chains:
-            self.commits[chain] = self.commits[chain].rolling(4).mean()
-        self.commits.columns = ['Date'] + self.target_names
-        df1 = self.commits.melt('Date', var_name = 'Protocol',  value_name = 'Commits')
-        fig1 = sns.lineplot(x = "Date", y = "Commits", hue = 'Protocol', data = df1)
-        fig1.get_figure().savefig('commits.png')
-        fig1.clear()
-        
-    def plot_churn(self):
-        # Compute a 4-period MA to smooth data
-        for chain in self.chains:
-            self.churn[chain] = self.churn[chain].rolling(4).mean()
-        self.churn.columns = ['Date'] + self.target_names
-        df2 = self.churn.melt('Date', var_name = 'Protocol',  value_name = 'Churn')
-        fig2 = sns.lineplot(x = "Date", y = "Churn", hue = 'Protocol', data = df2)
-        fig2.set_yscale("log") # Large additions e.g. opening a repo makes the linear data hard to interpret
-        fig2.get_figure().savefig('churn.png')
-        fig2.clear()
-    
-    def plot_devs(self):
-        protocols_comparison = pd.DataFrame({'Month': ['Jun 19', 'Jul 19', 'Aug 19', 'Sep 19', 'Oct 19', 'Nov 19', 'Dec 19', 'Jan 20', 'Feb 20', 'Mar 20', 'Apr 20', 'May 20']})
+            with open(chain + '_history.json') as json_file:
+                data = json.load(json_file)
+            self.commits[chain] = data['weekly_commits']
+            self.churn[chain] = data['weekly_churn'][-52:]
+        sns.set(style = "darkgrid")
+        sns.set(rc = {'figure.figsize': (16, 9)})
+
+    def prep_code(self, commits_or_churn : str = 'commits'):
+        if commits_or_churn == 'commits':
+            commits_or_churn_df = self.commits
+        elif commits_or_churn == 'churn':
+            commits_or_churn_df = self.churn
+        else:
+            print('Usage: plot_code must be called on commits or churn.')
+            sys.exit(1)
+        percentage_changes = pd.DataFrame({'Protocol': self.target_names})
+        change_list = []
+        for index, chain in enumerate(self.chains):
+            if commits_or_churn_df[chain].mean() < 10:
+                # Negligible commits or churn, dead protocol
+                print(commits_or_churn.capitalize() + ': ' + self.target_names[index] + ' averaged fewer than 10 changes per week and can be considered a dead protocol.')
+                percentage_changes = percentage_changes[percentage_changes.Protocol != self.target_names[index]]
+                continue
+            try:
+                # Average of first and last 8 weeks
+                percentage_change = round(((sum(commits_or_churn_df[chain][-9:-1]) / sum(commits_or_churn_df[chain][0:8])) * 100) - 100)
+            except:
+                # None at the start of the year
+                percentage_change = 0
+            change_list.append(percentage_change)
+            # Compute a 4-period MA to smooth data
+            commits_or_churn_df[chain] = commits_or_churn_df[chain].rolling(4).mean()            
+        commits_or_churn_df.columns = ['Date'] + self.target_names
+        percentage_changes['Percentage change in ' + commits_or_churn] = change_list
+        percentage_changes = percentage_changes.sort_values('Percentage change in ' + commits_or_churn)
+        code = commits_or_churn_df.melt('Date', var_name = 'Protocol',  value_name = commits_or_churn)
+        return code, percentage_changes
+
+    def prep_devs(self):
+        protocols_comparison = pd.DataFrame({'Month': self.xaxis})
         percentage_changes = pd.DataFrame({'Protocol': self.contributor_chains})
         change_list = []
         for chain in self.contributor_chains:
@@ -60,30 +75,45 @@ class Visualize:
         percentage_changes['Protocol'] = self.contributor_target_names
         percentage_changes['Percentage change in active devs'] = change_list
         percentage_changes = percentage_changes.sort_values('Percentage change in active devs')
-        print(percentage_changes)
         protocols_comparison = protocols_comparison.melt('Month', var_name = 'Protocol', value_name = 'Monthly Active Devs')
-        # Disable Seaborn sorting or months appear out of order
-        fig3 = sns.lineplot(x = "Month", y = "Monthly Active Devs", hue = 'Protocol', data = protocols_comparison, sort = False)
-        fig3.get_figure().savefig('devs.png')
-        fig3.clear()
-        fig3 = sns.barplot(y = "Protocol", x = "Percentage change in active devs", data = percentage_changes, palette = "RdYlGn")
-        fig3.get_figure().savefig('devchange.png')
-        fig3.clear()
-        
-            
-    
-    def run(self):
-        for chain in self.chains:
-            self.load(chain)
-        sns.set(style = "darkgrid")
-        sns.set(rc = {'figure.figsize': (16, 9)})
-        self.plot_churn()
-        self.plot_commits()
+        return protocols_comparison, percentage_changes
 
+    # Each of the below is separate as seaborn requires plots to have different figure variable names and clearing memory completely is not possible.
+
+    def plot_commits(self, code : pd.DataFrame, percentage_changes: pd.DataFrame):
+        fig1 = sns.lineplot(x = "Date", y = 'commits', hue = 'Protocol', data = code)
+        fig1.get_figure().savefig('commits.png')
+        fig1.clear()
+        fig2 = sns.barplot(y = "Protocol", x = "Percentage change in commits", data = percentage_changes, palette = "RdYlGn")
+        fig2.get_figure().savefig('commits_change.png')
+        fig2.clear()
+
+    def plot_churn(self, code : pd.DataFrame, percentage_changes: pd.DataFrame):
+        fig3 = sns.lineplot(x = "Date", y = 'churn', hue = 'Protocol', data = code)
+        fig3.set_yscale("log")
+        fig3.get_figure().savefig('churn.png')
+        fig3.clear()
+        fig4 = sns.barplot(y = "Protocol", x = "Percentage change in churn", data = percentage_changes, palette = "RdYlGn")
+        fig4.get_figure().savefig('churn_change.png')
+        fig4.clear()
+
+    def plot_devs(self, protocols_comparison : pd.DataFrame, percentage_changes : pd.DataFrame):
+        # Disable Seaborn sorting or months appear out of order
+        fig5 = sns.lineplot(x = "Month", y = "Monthly Active Devs", hue = 'Protocol', data = protocols_comparison, sort = False, palette="Dark2_r")
+        fig5.get_figure().savefig('devs.png')
+        fig5.clear()
+        fig6 = sns.barplot(y = "Protocol", x = "Percentage change in active devs", data = percentage_changes, palette = "RdYlGn")
+        fig6.get_figure().savefig('devs_change.png')
+        fig6.clear()
+        
+    def run(self):
+        code, percentage_changes = self.prep_code('commits')
+        self.plot_commits(code, percentage_changes)
+        code, percentage_changes = self.prep_code('churn')
+        self.plot_churn(code, percentage_changes)
+        protocols_comparison, percentage_changes = self.prep_devs()
+        self.plot_devs(protocols_comparison, percentage_changes)
 
 if __name__ == '__main__':
     v = Visualize()
-    sns.set(style = "darkgrid")
-    sns.set(rc = {'figure.figsize': (16, 9)})
-    v.plot_devs()
-    #v.run()
+    v.run()
