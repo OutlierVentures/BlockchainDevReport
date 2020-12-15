7c5a62d3db5901d003d9de11434ee78fb2188367
Mudit Marda <muditmarda109@gmail.com>
2020-12-15 15:00:24 +0530

    Add shell script to generate reports

---
+++ generateReports.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+names=$(awk -F "=" '/names/ {print $2}' config.ini)
+
+rm logfile.log
+
+echo "Generating reports ..."
+
+python3 updateProtocols.py
+
+echo "Running dev.py ..."
+for name in $names; do
+    python3 dev.py $name
+done
+
+echo "Running contr.py ..."
+for name in $names; do
+    python3 contr.py protocols/$name.toml
+done
+
+# echo "Running visualizer ..."
+# python3 vis.py
